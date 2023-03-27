6c553c8ff856d734920b35063e0955e68715da6d
axithorn <128606408+axithorn@users.noreply.github.com>
2023-03-27 18:04:49 +0100

    Add files via upload

--- traffic_light.js
+++ traffic_light.js
@@ -1,23 +1,25 @@
-function updateTrafficLight() {
-  ZOHO.CRM.API.getRecord({Entity:"Contacts",RecordID:ZOHO.CRM.ENTITY.getID()})
-  .then(function(response){
-    var TE_Score = response.data[0].TE_Score;
-    var trafficLightElement = document.getElementById("traffic-light");
+ZOHO.embeddedApp.init().then(function() {
+  function updateTrafficLight() {
+    ZOHO.CRM.API.getRecord({Entity:"Contacts",RecordID:ZOHO.CRM.ENTITY.getID()})
+    .then(function(response){
+      var TE_Score = response.data[0].TE_Score;
+      var trafficLightElement = document.getElementById("traffic-light");
 
-    if (trafficLightElement === null) {
-      return;
-    }
+      if (trafficLightElement === null) {
+        return;
+      }
 
-    if (TE_Score >= 0 && TE_Score <= 50) {
-      trafficLightElement.style.backgroundColor = "red";
-    } else if (TE_Score > 50 && TE_Score <= 75) {
-      trafficLightElement.style.backgroundColor = "orange";
-    } else if (TE_Score > 75) {
-      trafficLightElement.style.backgroundColor = "green";
-    } else {
-      trafficLightElement.style.backgroundColor = "transparent";
-    }
-  });
-}
+      if (TE_Score >= 0 && TE_Score <= 50) {
+        trafficLightElement.style.backgroundColor = "red";
+      } else if (TE_Score > 50 && TE_Score <= 75) {
+        trafficLightElement.style.backgroundColor = "orange";
+      } else if (TE_Score > 75) {
+        trafficLightElement.style.backgroundColor = "green";
+      } else {
+        trafficLightElement.style.backgroundColor = "transparent";
+      }
+    });
+  }
 
-window.onload = updateTrafficLight;
+  window.onload = updateTrafficLight;
+});
